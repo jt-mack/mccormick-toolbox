67e95fcf9e02b24990ce694e7f49cddf417edc2d
jmccormick@tarkenton.com <jmccormick@tarkenton.com>
2024-11-21 16:54:26 -0500

    added wingap source types

---
+++ electron/services/database/schema/wingap/mappings/land-classification.ts
@@ -0,0 +1,28 @@
+import type {DbTable} from "../../common";
+import {SUBDIVISEntity} from "../source/Database";
+
+import {LandClassification} from "@models/entities/land-classification";
+import { type KeyMapper} from "../../transformers.ts";
+
+
+export const landClassMapper:KeyMapper<LandClassification, SUBDIVISEntity>={
+  id:'SUBDIVCODE',
+  description:'SUBDIVNAME',
+  method:'CALCMETHOD',
+}
+
+
+export const landClassification: DbTable = {
+  name: 'SUBDIVIS',
+  primaryKey: 'SUBDIVCODE',
+  columns: [
+    'id',
+    'name',
+    'description',
+    'created_at',
+    'updated_at',
+  ],
+}
+
+
+
